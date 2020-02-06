765a809ebc571adc074179c69d150bc14915c1ad
Rajiv Gupta <rajiv_gupta@intuit.com>
2020-02-06 12:33:09 +0530

    Initial Commit

---
+++ src/main/java/com/tracker/LastSeen/LastSeenApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.LastSeen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastSeenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastSeenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/LastSeen/LastSeenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.LastSeen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastSeenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
